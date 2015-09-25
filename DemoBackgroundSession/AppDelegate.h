323f39b9d3cddccd990d94212f21d2deb21cf7fe
Александр О. Кургин <kurgin@yamoney.ru>
2015-09-25 12:56:49 +0300

    Initial Commit

---
+++ DemoBackgroundSession/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoBackgroundSession
+//
+//  Created by Александр О. Кургин on 25.09.15.
+//  Copyright (c) 2015 Yandex.Money. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
